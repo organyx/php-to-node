fca89d6acf1b73a818f027b18d3891fb47ab0c61
AleksBeliun <aleksandr@nvoresume.com>
2023-06-25 18:58:27 +0200

    feat: :zap: Add validation to POST and PUT routes

--- routes/v1/product.router.js
+++ routes/v1/product.router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ProductController = require('../../controllers/v1/product.controller');
+const validate = require('../../middleware/validate');
+const { createProductSchema, updateProductSchema } = require('../../validation-schemas/product.schema');
 
-router.post('/', ProductController.createProduct);
+router.post('/', validate(createProductSchema), ProductController.createProduct);
 router.get('/', ProductController.getProducts);
 router.get('/:id', ProductController.getProductById);
-router.put('/:id', ProductController.updateProduct);
+router.put('/:id', validate(updateProductSchema), ProductController.updateProduct);
 router.delete('/:id', ProductController.deleteProduct);
 
 module.exports = router;
